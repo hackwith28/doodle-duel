03543e2a67b69d3f507c3dc443ff6ed78054b978
hackwith28 <reeteshsahu1828@gmail.com>
2026-01-10 20:15:04 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io(" https://doodle-duel-1.onrender.com
+==> ", {
   transports: ["websocket"],
   reconnection: true,
   autoConnect: true,
