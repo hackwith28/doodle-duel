53082076ead0913c22d3807649eee6a72d82b9a1
hackwith28 <reeteshsahu1828@gmail.com>
2026-06-26 21:33:50 +0530

    fix socket transport for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["polling", "websocket"],
   reconnection: true,
   autoConnect: false,
 });
